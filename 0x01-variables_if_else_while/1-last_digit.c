b23666d822c905cfff5873c414f7046b823bdaf8
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-17 16:22:09 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,27 +2,33 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ *main - Entry point
+ *
+ * Description: 'Evaluate the last number of random input'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 	int ln;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ln = n % 10;
-	if (n > 5)
+	if (ln > 5)
 		{printf("Last digit of%d ", n);
 		printf(" is%d", ln);
-		printf(" and is greater than 5\n");
-		}
-	else if (n== 0)
-		{printf("Last digit of%d", n);
-		printf(" is%d", ln);
-		printf(" and is 0\n");}
+		printf(" and is greater than 5\n "); }
+	else if (ln == 0)
+		{printf("Last digit of%d ", n);
+		printf(" is%d ", ln); }
 
-	else if	(n < 6 && 0)
-		{printf("Last digit of %d", n);
-		printf(" is%d", ln);
-		printf(" and is less than 6 and not 0\n");}
+	else if	(ln < 6 && ln > 0)
+		{printf("Last digit of %d ", n);
+		printf(" is%d ", ln);
+		printf(" and is less than 6 and not 0\n"); }
+
 	return (0);
 }
