6f8b85ed9c1d7a84a05bf22cc7350831ca579cbb
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-21 15:56:56 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0 (Success)
 *
-* */
+*/
 
 	void print_alphabet_x10(void)
 	{	int i;
@@ -17,9 +17,6 @@
 	{
 	for (j = 97; j <= 122; j++)
 		{_putchar(j); } }
-	_putchar('\n');
 	
+
 	}
-        
-
-
