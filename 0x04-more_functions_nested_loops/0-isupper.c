7d6807297771b35bf4f6512c78202a8b45da8244
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-21 04:36:16 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+	int _isupper(int c)
+
+{
+	for (c = 65; c <= 122; c++)
+	{
+		_putchar('%d'c);
+	}
+	if (c <= 90 && c => 65)
+		{
+	
+			return (1);
+
+		}
+	else
+		{
+			return (0);
+		}
+
+
+}
