db083c40aa143933ef4a37562484ff03a66b4107
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-21 14:26:46 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -20,3 +21,5 @@ void jack_bauer(void);
 void times_table(void);
 
 void print_to_98(int n);
+#endif /* MAIN_H*/
+
