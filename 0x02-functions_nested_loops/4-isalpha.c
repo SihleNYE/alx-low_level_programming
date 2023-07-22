8137758edd8126c75a963d9f3ce597abb51f1147
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-21 17:49:14 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,30 +2,24 @@
 
 
 /**
- *_isalpha -  checks for alphabetic charac
- *@c returns ascii value 
+ *_isalpha -  checks for alphabetic character
+ *@c returns ascii value
  * Description: 'Returns zero if not uppercase'
  *
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-	
 
 	if (c >= 65 && c <= 90 )
-	
+
 	{	return (1); }
-	
+
 	if (c <= 97 && c >= 122)
-	
+
 	{	return (1);
-		
-		};
+
+	}
 
 	return (0);
-		
-        
-
-
-
 }
