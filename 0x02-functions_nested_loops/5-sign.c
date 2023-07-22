b115aae496b896324176f5e48bdbff3af4d1fe79
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-21 18:38:46 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,13 +14,14 @@
 
 if (n > 0)
 		{_putchar('+');
-		return (1);}
+		return (1); }
 if (n == 0)
 		{_putchar('0');
-			return (0);}
+			return (0); }
 
-if (n < 0)	{_putchar('-');
-			return (- 1);}
+if (n < 0)
+		{_putchar('-'); 
+			return (-1); }
 
 	_putchar('\n');
 	return (0);
