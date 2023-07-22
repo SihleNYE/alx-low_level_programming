e08bae6e36d1650ee8d07d9a48197cc811b7395a
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-21 17:02:33 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,16 +4,16 @@
  *_islower- Entry point
  *
  * Description: 'checks for lowercase character and return 1'
- *
+ *@c :integer returns ascii value
  * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {	c = 0;	
 
-	if(c > 97 && c < 123)
+	if (c > 97 && c < 123)
 	{return (1); }
 
 	else
-	{return(0);}
+	{return (0); }
 }
