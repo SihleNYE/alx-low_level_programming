abeaf010f57061c6a17afce21f442aaaea41024b
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-23 09:28:55 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
-
 /**
- *Print_last_digit - Calculate and print last digit
+ * print_last_digit - Entry point
  *
- *Description: ' Calculate Print Last digit of number'
+ * Description: 'Calc and Print last digit of number'
  *
- *n is assigned to a value called results,that value has
- * the modulus operation of n % 10,which grabs the last dig
- *it of the n value.Finally we return result and add 48
- *which is the ascii value of 0 ar the add of the digit.
+ *@n :integer stores number value to calculate
  *
- *@n : store value of last digit
+ * Return: Results (Last Value)
  *
- *Return: Results ,the last value of digit.
  */
 
 int print_last_digit(int n)
