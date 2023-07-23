89a6e94c071ce6228e229d51ce1d2fa2854fb045
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-23 13:14:45 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,23 +2,55 @@
 
 
 /**
- *main - Entry point
+ *times_table - Entry point
  *
- * Description: ''
+ * Description: 'Display 9 x 1 to 9 x 9 table'
  *
- * Return: Always 0 (Success)
+ * Return: Void/Nothing
  */
 
 void times_table(void)
 {
 
+	int Num1;
+	int Num2;
+	int rslt;
+	int LstDgt;
+	int Mtply;
 
-for (int i =0;i < 10;i++)
-	{	
-		for (int row =i ;row > 10; row++);
-		_putchar(i);
+	for (Num1 = 0; Num1 <= 9; Num1++)
+	{
+	for (Num2 = 0; Num2 <= 9; Num2++)
+	{
+		rslt = Num1 * Num2;
+	if (rslt > 9)
+	{
+		LstDgt = rslt % 10;
+		Mtply = (rslt - LstDgt) / 10;
+		_putchar(44);
+		_putchar(32);
+		_putchar(Mtply + 48);
+		_putchar(LstDgt + 48);
+	}
+	else
+	{
+		if (Num2 != 0)
+		{
+		_putchar(44);
+		_putchar(32);
+		_putchar(32);
+		}
+		_putchar(48);
+	}
+
 
 	}
-	_putchar('\n');
-	return (void);
+	}
+
+		_putchar('\n');
+
 }
+
+
+
+
