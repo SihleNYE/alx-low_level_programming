dcf2908926051d8d3b366156d2722b918354c49d
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-23 07:32:12 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,22 +8,17 @@
  *
  * Return: Always 0 (Success)
  */
-void jack_bauer(void){
+void jack_bauer(void)
+{
 	int min;
 	int hour;
 
-	for (min =0; hour <24 ;hour++)
+	for (hour =0; hour < 24; hour++)
 
-		{
-			for (min = 0; min <= 59; min++)
-				{
-					_putchar(hour);
-					_putchar(':');
-					_putchar(min);
-
-				}
-		}
-	
-
+	{for (min = 0; min <= 59; min++)
+	{_putchar(hour);
+	_putchar(':');
+	_putchar(min); }
+_putchar('\n'); }
 
 }
