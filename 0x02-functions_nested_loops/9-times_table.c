cfd0d9296af627a4277409f475e6df387ea67c87
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-23 13:34:48 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,37 +20,16 @@ void times_table(void)
 
 	for (Num1 = 0; Num1 <= 9; Num1++)
 	{
-	for (Num2 = 0; Num2 <= 9; Num2++)
-	{
-		rslt = Num1 * Num2;
-	if (rslt > 9)
-	{
-		LstDgt = rslt % 10;
-		Mtply = (rslt - LstDgt) / 10;
-		_putchar(44);
-		_putchar(32);
-		_putchar(Mtply + 48);
-		_putchar(LstDgt + 48);
-	}
-	else
-	{
-		if (Num2 != 0)
+		for (Num2 = 0; Num2 <= 9; Num2++)
 		{
-		_putchar(44);
-		_putchar(32);
-		_putchar(32);
+			rslt = Num1 * 9;
+			_putchar(rslt);
 		}
-		_putchar(48);
+
+			_putchar('\n');
 	}
 
 
-	}
-	}
-
-		_putchar('\n');
 
 }
-
-
-
 
