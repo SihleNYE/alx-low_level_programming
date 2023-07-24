dca714deccdf7d3e606ebf6786a94202b2aed710
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-24 15:04:36 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,18 +3,18 @@
 /**
  *print_to_98 - 'Print from input up to 98'
  *@n : input to increment
- * 
+ *
  *
  * Return: void
  */
 	void print_to_98(int n)
 {
-	do	
-		{_putchar(n);
-		_putchar(\n);
+	do {
+		_putchar(n);
+		_putchar('\n');
+
+		n++;
 		}
-	n++;
-	while (n < 99)
-	
-	
+	while (n < 99);
+
 }
