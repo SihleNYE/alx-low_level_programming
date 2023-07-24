e89aaa365dadc364a6afb3843d0cd69eae6ace13
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-24 08:58:08 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen - get string length
+ *
+ * @s : stores chars from all string
+ * Return: int length of string
+ */
+
+	int _strlen(char *s)
+
+{
+	int slength = 0;
+
+	while (*s != '\0')
+	{
+		slength++;
+		*s++;
+	}
+	return (slength);
+}
