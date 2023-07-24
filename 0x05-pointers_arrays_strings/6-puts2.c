11760fff581325d561c15db37183246ad23e8760
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-24 11:05:41 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - prints inputs value from string
+ *@str :  stores return value
+ * Return: Void.
+ */
+
+	void puts2(char *str)
+{
+	int slength = 0;
+	int arrSize = 0;
+	char *addr = str;
+	int rslt;
+
+	while (*addr != '\0')
+	{
+		*addr++;
+		slength++;
+	}
+		arrSize = slength - 1;
+
+	for (rslt = 0; rslt <= arrSize; rslt++)
+	{
+		if (rslt % 2 == 0)
+		{
+			_putchar(str[rslt]);
+		}
+	}
+			_putchar('\n');
+}
