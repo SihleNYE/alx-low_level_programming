f3725ecc632844c9518e243c0a9741c4df37a562
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-25 13:07:35 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line- print straight line
+ *@n : input
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	char c;
+	c = 47;
+
+	for (n = 0; n > 0; n++)
+	{
+	if (n > 0)
+		{ _putchar(c); }
+
+	else
+	{ _putchar('\n'); }
+	}
+
+
+
+
+}
