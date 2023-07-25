a5d955379dd478f819a814ab1c670c80d0cdfe3b
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-25 08:01:36 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print (1-9) and new line
+ *
+ * Return: Void
+ */
+
+	void print_numbers(void)
+{
+	char c;
+
+	for  (c = 48; c <= 59; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+
+}
