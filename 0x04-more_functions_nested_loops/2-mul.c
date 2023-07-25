d5fd4328eae7dfb971e18cee3c8a10b9ac4c328c
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-25 07:18:20 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul- multiply two integers
+ *@a: input 
+ @b : input to multiply 
+ * Return: Success  Always Total
+ */
+
+	int mul(int a, int b)
+{
+
+	int pro;
+
+	pro = int a * int b;
+
+	return (pro);
+}
