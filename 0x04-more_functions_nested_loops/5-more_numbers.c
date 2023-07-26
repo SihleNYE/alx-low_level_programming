3fa304864110626be608670b3f32d04dc3164bbe
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-25 18:00:29 -0700

    Task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,29 +2,37 @@
 
 
 /**
+
  * more_numbers - print more numbers
- * Return:Void
+
  */
 
 
 void more_numbers(void)
 
 {
-	int i;
-	int j;
+
+        int i, j;
 
 
-	for (i = 1; i <= 10; i++)
-	{
-	for (j = 0; j <= 14; j++)
-		{
-		if (j >= 10)
-			_putchar('1');
-			_putchar (j % 10 + '0');
-		}
-			_putchar('\n');
-	}
+        for (i = 1; i <= 10; i++)
+
+        {
+
+                for (j = 0; j <= 14; j++)
+
+                {
+
+                        if (j >= 10)
+
+                                _putchar('1');
+
+                        _putchar (j % 10 + '0');
+
+                }
+
+                _putchar('\n');
+
+        }
 
 }
-
-
