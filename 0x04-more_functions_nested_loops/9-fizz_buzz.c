612dcd1ec48c8c37c00981c7d6622ef02710d30c
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-25 17:22:08 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *Fizz_Buzz : 'Print 1-100 with fizzbuzz in between'
+ *Return : Success Always (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+
+		if (n % 3 == 0 && n % 5 == 0)
+		{	printf(" fizzbuzz "); }
+		else if (n % 5 == 0)
+		{printf("buzz "); }
+		else if (n % 3 == 0)
+		{printf(" fizz "); }
+		else
+		printf(" %d ", n);
+
+	}
+	printf("\n");
+return (0);
+}
+
