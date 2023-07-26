7a9a5148ec03fe6c07c7e3c81ff136f516649483
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-25 17:24:48 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 		if (n % 3 == 0 && n % 5 == 0)
 		{	printf(" fizzbuzz "); }
 		else if (n % 5 == 0)
-		{printf("buzz "); }
+		{printf(" buzz "); }
 		else if (n % 3 == 0)
 		{printf(" fizz "); }
 		else
