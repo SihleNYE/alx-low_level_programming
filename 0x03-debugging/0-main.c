b2a864c2e04ecb931a5364220a19cdbcc18e2add
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-26 09:37:26 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry Point
+ * main - tests function that prints if integer is positive or negative
  *Return: Always 0
  *
  */
 
 int main(void)
-{
-	int i;
+{	int i;
 
 	i = 0;
-	void positive_or_negative(int i);
+	positive_or_negative(i);
+
 	return (0);
 }
