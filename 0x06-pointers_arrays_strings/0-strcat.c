5f5f93b10c40e0328b4c2ec3a096c9e97272085f
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-26 20:02:42 -0700

    task zero

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,53 +3,40 @@
 /**
 
  * _strcat - concatenates two strings
-
  * @dest: input value
-
  * @src: input value
-
- *
-
- * Return: void
-
+ * Return: char
  */
 
 char *_strcat(char *dest, char *src)
 
 {
 
-        int i;
+	int i;
 
-        int j;
+	int j;
 
+	i = 0;
 
-        i = 0;
+	while (dest[i] != '\0')
 
-        while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-        {
+		j = 0;
 
-                i++;
+	while (src[j] != '\0')
 
+	{
+	dest[i] = src[j];
+
+		i++;
+		j++;
         }
 
-        j = 0;
+	dest[i] = '\0';
 
-        while (src[j] != '\0')
-
-        {
-
-                dest[i] = src[j];
-
-                i++;
-
-                j++;
-
-        }
-
-
-        dest[i] = '\0';
-
-        return (dest);
+	return (dest);
 
 }
