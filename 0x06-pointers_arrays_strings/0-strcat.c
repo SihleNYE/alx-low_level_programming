08728d9bc522a1819b2dc98eb31efc0d2a7708a5
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-26 19:27:57 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concetenate two strings/join two strings into one.
+ *@dest : input
+ *@src : input
+ *Return: combined string
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	
+	j = 0;
+	while (src[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
