0d0806c8b502b15e433e265ad9237e1e4efa1550
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-28 06:55:47 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,14 @@ char *cap_string(char *str)
 {
 
 		int index = 0;
-	
+
 	while (str[index])
 
 	{
 
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 
-		index++;
+		{index++;
 
 		if (str[index - 1] == ' ' ||
 		str[index - 1] == '\t' ||
@@ -51,7 +51,8 @@ char *cap_string(char *str)
 		str[index] -= 32;
 
 
-		index++;
+		{index++;}
+		}
 
 	}
 
