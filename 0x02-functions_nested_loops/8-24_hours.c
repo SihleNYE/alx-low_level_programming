b693597b3820d3f696cd21e8feb6662c59477b59
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-29 13:04:38 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,36 +8,39 @@
  *
  * Return: Void
  */
-void jack_bauer(void)
-{
-	int m1;
-	int m2;
-	int s1;
-	int s2;
 
-	for (m1 = 0; m1 <= 2; m1++)
+void jack_bauer(void)
+
+{
+
+	int i, j;
+
+
+		i = 0;
+
+
+	while (i < 24)
+
+	{j = 0;
+
+	while (j < 60)
 
 	{
-		for (m2 = 0; m2 <= 9; m2++)
-		{
-			for (s1 = 0; s1 <= 5; s1++)
+	_putchar((i / 10) + '0');
 
-			{
-				for (s2 = 0; s2 <= 9; s2++)
-				{
-					
-					if (m1 <= 2 && m2 >= 4)
-					break;
-				_putchar(m1 + 48);
-				_putchar(m2 + 48);
-				_putchar(58);
-				_putchar(s1 + 48);
-				_putchar(s2 + 48);
-				_putchar('\n');
-				}
-			}
-		}
+	_putchar((i % 10) + '0');
+
+	_putchar(':');
+
+	_putchar((j / 10) + '0');
+
+	_putchar((j % 10) + '0');
+
+	_putchar('\n');
+
+		j++; }
+		i++;
+
 	}
-
 
 }
