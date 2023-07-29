c05beb9be6155ee2caa98e06e930b27749a549ae
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-28 19:32:02 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
-int main(void)
 
-	for (char c = '0',c >
+	int main(void)
+
+{
+
+		int n;
+
+
+	for (n = 48; n < 58; n++)
+
+	{
+
+		putchar(n);
+
+		if (n != 57)
+
+		{
+
+		putchar(',');
+
+		putchar(' ');
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
