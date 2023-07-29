83a378ab8403c133aad5e39fbd7a7c34ac800747
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-29 12:02:23 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -14,11 +14,11 @@
 
 		if (n > 0)
 
-		_putchar('+');
+		{_putchar('+');
 
-		return (1);
-
-		} else if (n == 0)
+		return (1); } 
+		
+		else if (n == 0)
 
 		{
 
@@ -26,7 +26,9 @@
 
 		return (0);
 
-		} else if (n < 0)
+		} 
+		
+		else if (n < 0)
 		{
 
 			_putchar('-');
