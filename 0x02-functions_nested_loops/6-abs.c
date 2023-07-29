682cfb0d9387ad600134de4a8df2be7674fb0a05
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-29 12:33:54 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,20 +10,17 @@
  */
 
 
-	
-        int _abs(int n)
 
-        {
+		int _abs(int n)
 
-                if (n >= 0)
+{
+		if (n >= 0)
 
-                {
+		{
+			return (n);
+		}
 
-                        return (n);
+	return (-n);
 
-                }
-
-                return (-n);
-
-        }
+}
 
