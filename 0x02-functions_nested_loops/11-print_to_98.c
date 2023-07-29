0bdfe01f6c07b8c6516fdbbbbd16804366ec12fd
SihleNYE <sihlejeromenyendwana@gmail.com>
2023-07-29 14:04:21 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,32 +5,32 @@
  *print_to_98 - 'Print from input up to 98'
  *@n : input to increment
  *
- *
  * Return: void
  */
-	void print_to_98(int n)
+
+void print_to_98(int n)
+
 {
-	if (n > 98)
-	{
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-			printf(", ");
-		}
-	}
+int i, j;
 
-	if (n < 98)
-	{
-		for (; n <= 98; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-
-	if (n == 98)
-	{
-		printf("%d", n);
-	}
-
-		printf("\n");
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
+}
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
 }
